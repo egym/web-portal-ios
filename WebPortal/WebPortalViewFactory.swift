23f10af6b25cba81659f39be693ba768735e3792
Achraf Ameddah <achraf.ameddah@egym.com>
2022-11-17 15:10:00 +0100

    Add immediate sync after update (#6)
    * Add bool value for immediate sync
    
    * Update web portal view factory to reload view when new update is available
    
    * Refactor code

--- WebPortal/WebPortalViewFactory.swift
+++ WebPortal/WebPortalViewFactory.swift
@@ -5,32 +5,64 @@
 //  Created by Maksym Kravchenko on 18.05.2022.
 //
 
-import IonicPortals
 import IonicLiveUpdates
+import IonicPortals
 import UIKit
 
 public protocol WebPortalViewFactory {
-    func createView(config: PortalConfig, initialContext: [String: String]) -> UIView
+    func createView(config: PortalConfig, initialContext: [String: String], completion: @escaping (UIView) -> Void)
 }
 
 public extension WebPortalViewFactory {
-    func createView(config: PortalConfig, initialContext: [String: String]) -> UIView {
-        let liveUpdateConfig: LiveUpdate?
+    func createView(config: PortalConfig, initialContext: [String: String], completion: @escaping (UIView) -> Void) {
         if config.isLiveUpdateEnabled,
-            let liveAppId = config.liveAppId,
-            let liveUpdateChannel = config.liveUpdateChannel {
-            liveUpdateConfig = LiveUpdate(appId: liveAppId, channel: liveUpdateChannel)
+           let liveAppId = config.liveAppId,
+           let liveUpdateChannel = config.liveUpdateChannel {
+            guard config.syncImmediately else {
+                let liveUpdateConfig = LiveUpdate(appId: liveAppId, channel: liveUpdateChannel)
+                let portal = Portal(
+                    name: config.name,
+                    startDir: config.startDir,
+                    initialContext: initialContext,
+                    liveUpdateConfig: liveUpdateConfig
+                )
+                completion(PortalUIView(portal: portal))
+                return
+            }
+
+            let updateManager = LiveUpdateManager.shared
+            let liveUpdateConfig = LiveUpdate(
+                appId: liveAppId,
+                channel: liveUpdateChannel,
+                syncOnAdd: false
+            )
+
+            try? updateManager.add(liveUpdateConfig)
+            let portal = Portal(
+                name: config.name,
+                startDir: config.startDir,
+                initialContext: initialContext,
+                liveUpdateManager: updateManager,
+                liveUpdateConfig: liveUpdateConfig
+            )
+
+            let portalView = PortalUIView(portal: portal)
+            // Show static content while loading new update if available
+            completion(portalView)
+
+            updateManager.sync(appId: liveAppId) { _ in
+                DispatchQueue.main.async {
+                    portalView.reload()
+                }
+            }
         } else {
-            liveUpdateConfig = nil
+            let portal = Portal(
+                name: config.name,
+                startDir: config.startDir,
+                initialContext: initialContext
+            )
+
+            completion(PortalUIView(portal: portal))
         }
-
-        let portal = Portal(
-            name: config.name,
-            startDir: config.startDir,
-            initialContext: initialContext,
-            liveUpdateConfig: liveUpdateConfig
-        )
-
-        return PortalUIView(portal: portal)
     }
 }
