ed6b95afd4ec5b9305cb207871469d9cda485f01
Achraf Ameddah <achraf.ameddah@egym.com>
2022-11-17 14:13:19 +0100

    Refactor code

--- WebPortal/WebPortalViewFactory.swift
+++ WebPortal/WebPortalViewFactory.swift
@@ -17,8 +17,7 @@ public extension WebPortalViewFactory {
     func createView(config: PortalConfig, initialContext: [String: String], completion: @escaping (UIView) -> Void) {
         if config.isLiveUpdateEnabled,
            let liveAppId = config.liveAppId,
-           let liveUpdateChannel = config.liveUpdateChannel
-        {
+           let liveUpdateChannel = config.liveUpdateChannel {
             guard config.syncImmediately else {
                 let liveUpdateConfig = LiveUpdate(appId: liveAppId, channel: liveUpdateChannel)
                 let portal = Portal(
@@ -51,27 +50,19 @@ public extension WebPortalViewFactory {
             // Show static content while loading new update if available
             completion(portalView)
 
-            if config.syncImmediately {
-                updateManager.sync(appId: liveAppId) { _ in
-                    DispatchQueue.main.async {
-                        portalView.reload()
-                    }
+            updateManager.sync(appId: liveAppId) { _ in
+                DispatchQueue.main.async {
+                    portalView.reload()
                 }
             }
-        }
-        // Live update is disabled, create static view from `startDir`
-        else {
+        } else {
             let portal = Portal(
                 name: config.name,
                 startDir: config.startDir,
                 initialContext: initialContext
             )
 
-            completion(
-                PortalUIView(
-                    portal: portal
-                )
-            )
+            completion(PortalUIView(portal: portal))
         }
     }
 }
