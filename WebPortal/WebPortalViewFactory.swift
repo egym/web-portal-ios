25a8313d010eb60d8f3bc30f50f7d66286e73c1c
Achraf Ameddah <achraf.ameddah@egym.com>
2022-11-30 15:13:34 +0100

    Refactor web view factory

--- WebPortal/WebPortalViewFactory.swift
+++ WebPortal/WebPortalViewFactory.swift
@@ -5,7 +5,6 @@
 //  Created by Maksym Kravchenko on 18.05.2022.
 //
 
-import IonicLiveUpdates
 import IonicPortals
 import UIKit
 
@@ -15,73 +14,12 @@ public protocol WebPortalViewFactory {
 
 public extension WebPortalViewFactory {
     func createView(config: PortalConfig, initialContext: [String: String], completion: @escaping (UIView) -> Void) {
-        if config.isLiveUpdateEnabled,
-           let liveAppId = config.liveAppId,
-           let liveUpdateChannel = config.liveUpdateChannel
-        {
-            guard config.syncImmediately else {
-                let liveUpdateConfig = LiveUpdate(appId: liveAppId, channel: liveUpdateChannel)
-                let portal = Portal(
-                    name: config.name,
-                    startDir: config.startDir,
-                    initialContext: initialContext,
-                    liveUpdateConfig: liveUpdateConfig
-                )
-                completion(PortalUIView(portal: portal))
-                return
-            }
+        let portal = Portal(
+            initialContext: initialContext,
+            config: config
+        )
 
-            let updateManager = LiveUpdateManager.shared
-            let liveUpdateConfig = LiveUpdate(
-                appId: liveAppId,
-                channel: liveUpdateChannel,
-                syncOnAdd: false
-            )
-
-            try? updateManager.add(liveUpdateConfig)
-            let portal = Portal(
-                name: config.name,
-                startDir: config.startDir,
-                initialContext: initialContext,
-                liveUpdateManager: updateManager,
-                liveUpdateConfig: liveUpdateConfig
-            )
-
-            let portalView = PortalUIView(portal: portal)
-            // Show static content while loading new update if available
-            completion(portalView)
-
-            checkForUpdate(liveAppId: liveAppId, portalView: portalView, updateManager: updateManager)
-
-        } else {
-            let portal = Portal(
-                name: config.name,
-                startDir: config.startDir,
-                initialContext: initialContext
-            )
-
-            completion(PortalUIView(portal: portal))
-        }
-    }
-
-    private func checkForUpdate(liveAppId: String, portalView: PortalUIView, updateManager: LiveUpdateManager) {
-        updateManager.checkForUpdate(appId: liveAppId) { result in
-            switch result {
-            case let .success(response):
-                if let snapshotId = response.data.snapshot {
-                    let localSnapshotsIds = updateManager.getAppSnapshots(for: liveAppId).map { $0.id }
-                    if localSnapshotsIds.contains(snapshotId) == false {
-                        updateManager.sync(appId: liveAppId) { _ in
-                            DispatchQueue.main.async {
-                                portalView.reload()
-                            }
-                        }
-                    }
-                }
-            case .failure:
-                // in case of failure, static view was already presented in the completion
-                break
-            }
-        }
+        let portalView = PortalUIView(portal: portal, config: config)
+        completion(portalView)
     }
 }
